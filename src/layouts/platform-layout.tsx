200a16908900620a4a60d3730737e2df249376dd
EdenR7 <edenrot777@gmail.com>
2025-04-29 17:13:18 +0300

    add footer

--- src/layouts/platform-layout.tsx
+++ src/layouts/platform-layout.tsx
@@ -2,15 +2,17 @@ import { Outlet } from "react-router-dom";
 
 import { MainNav } from "@/components/general/main-nav";
 import { CategoryNavbar } from "@/components/CategoryNavbar";
+import { Footer } from "@/components/general/footer";
 
 export default function PlatformLayout() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <MainNav />
       <CategoryNavbar />
-      <div className=" px-6 py-8 max-w-screen-2xl h-full 2xl:mx-auto">
+      <div className="px-6 py-8 max-w-screen-2xl w-full flex-1 2xl:mx-auto">
         <Outlet />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
